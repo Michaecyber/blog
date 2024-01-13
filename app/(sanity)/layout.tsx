87c3df111c26cf80850ddcf6f3d93de8d2bc5079
MichaelJs <147663415+Michaecyber@users.noreply.github.com>
2024-01-13 11:35:26 -0800

    first commit

---
+++ app/(sanity)/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'MMichale',
+  description: 'MMichael',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
